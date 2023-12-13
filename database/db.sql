47de0698a940c786ca8ff86935af3eacf844ea9e
Jhonathan Pimenta <jhonathanscp@gmail.com>
2023-12-13 07:41:38 -0300

    Fixing video bugs

--- database/db.sql
+++ database/db.sql
@@ -36,7 +36,7 @@ INSERT INTO courses
 ('lol', 'League of Legends', 'League of Legends é um renomado jogo online de batalha estratégica em equipe, no qual jogadores assumem o papel de campeões com habilidades únicas. Ambientado em um mundo fantástico, o objetivo é destruir o Nexus inimigo, enfrentando desafios táticos, trabalho em equipe e estratégias avançadas. Com uma comunidade global apaixonada e uma cena competitiva vibrante, LoL é um dos títulos mais populares no cenário de esports.', '/img/coursesimg/lol.jpg'),
 ('valorant', 'Valorant', 'Desenvolvido pela Riot Games, Valorant é um jogo tático de tiro em primeira pessoa que combina elementos de estratégia e habilidades especiais. Os jogadores assumem o papel de Agentes, cada um com habilidades únicas, enquanto competem em partidas intensas. A jogabilidade estratégica e a precisão são fundamentais, tornando Valorant um favorito entre os entusiastas de esports que apreciam desafios táticos e mecânicas de tiro precisas.', '/img/coursesimg/valorant.jpg'),
 ('fortnite', 'Fortnite', 'Fortnite é um jogo inovador de battle royale que se destaca por seu estilo artístico único, construção de estruturas e eventos épicos. Com 100 jogadores lutando pela sobrevivência em uma ilha em constante mudança, o jogo combina habilidade de tiro, estratégia de construção e um toque de humor. A comunidade global e os eventos regulares tornam Fortnite uma escolha emocionante para aqueles que buscam uma experiência dinâmica e competitiva.', '/img/coursesimg/fortnite.jpg'),
-('overwatch', 'DOTA 2', 'DOTA 2 é um jogo de estratégia em equipe que desafia os jogadores a controlar heróis poderosos em batalhas épicas. Com um mapa complexo, ampla variedade de heróis e um sistema de progressão estratégica, DOTA 2 é conhecido por sua profundidade estratégica. A cena competitiva é rica em torneios globais, oferecendo oportunidades para jogadores aprimorarem suas habilidades e competirem em níveis mais altos.', '/img/coursesimg/dota.jpg'),
+('dota2', 'DOTA 2', 'DOTA 2 é um jogo de estratégia em equipe que desafia os jogadores a controlar heróis poderosos em batalhas épicas. Com um mapa complexo, ampla variedade de heróis e um sistema de progressão estratégica, DOTA 2 é conhecido por sua profundidade estratégica. A cena competitiva é rica em torneios globais, oferecendo oportunidades para jogadores aprimorarem suas habilidades e competirem em níveis mais altos.', '/img/coursesimg/dota.jpg'),
 ('cs2', 'CS 2', 'CS 2 é um clássico jogo de tiro em primeira pessoa que se destaca pela jogabilidade tática, trabalho em equipe e habilidades de tiro precisas. Divididos em equipes de terroristas e contra-terroristas, os jogadores competem em partidas estratégicas para atingir objetivos específicos. Com uma cena competitiva robusta e uma base de jogadores dedicada, CS 2 é um título atemporal no cenário de esports.', '/img/coursesimg/cs.jpg');
 
 
@@ -74,7 +74,12 @@ CREATE TABLE comments (
 );
 
 INSERT INTO videos 
-(id, title, description, imgdir, course_id) VALUES
-('aula1_lol', 'Como kaitar no League of Legends', 'Hoje iremos ensinar a kaitar no League of Legends', '/img/videosimg/comokaitar.jpg', 'lol'),
-('aula1_dota2', 'Teleporte do vagabundo no DOTA 2', 'Aprenda o teleporte do desgraçado máximo com DinossauroHU3', '/img/videosimg/tpdovagabundo.jpg', 'dota2');
+(id, title, description, imgdir, xp_earned, course_id) VALUES
+('aula1_lol', 'Como kaitar no League of Legends', 'Hoje iremos ensinar a kaitar no League of Legends', '/img/videosimg/comokaitar.jpg', 300, 'lol'),
+('aula1_valorant', 'Como Dar Tapa Drop!', 'Meu HighLight muito top de valorant para ensina-los a jogar corretamente!', '/img/videosimg/aula1_valorant.png', 400, 'valorant'),
+('aula2_valorant', 'Como Destruir de AwP!', 'Hoje Vou Ensinalos A Destruir de AwP!', '/img/videosimg/aula2_valorant.png', 450, 'valorant'),
+('aula1_cs2', 'Como bangar a perfeitinha!', 'Hoje ensinarei pixels ROUBADISSIMOS DE BANGS no Cs2', '/img/videosimg/aula1_cs2.png', 500, 'cs2'),
+('aula1_dota2', 'Teleporte do vagabundo no DOTA 2', 'Aprenda o teleporte do desgraçado máximo com DinossauroHU3', '/img/videosimg/tpdovagabundo.jpg', 288, 'dota2')
+('aula1_fortnite', 'Como jogar fornite da maneira correta', 'Aprenda a jogar fortnite agora', '/img/videosimg/aula1_fortnite.jpg', 621, 'fortnite');
+
 
